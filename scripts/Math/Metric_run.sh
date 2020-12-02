8d84061c2b81091e6027f89f57831d696c1772a0
joel sunny <joelsunny.ch@gmail.com>
2020-12-02 23:27:21 +0530

    minor

--- scripts/Math/Metric_run.sh
+++ scripts/Math/Metric_run.sh
@@ -7,15 +7,8 @@ for i in {1..106}
 do
 bugid="$i"
 
-echo "checking out source"
-mkdir -p /tmp/sources/$projectname/"$bugid"f
-defects4j checkout -p $projectname -v "$bugid"f -w /tmp/sources/$projectname/"$bugid"f
-
 echo "collecting metrics"
 echo "./metric_collection_org.sh ${projectname} ${bugid}f"
 ../metric_collection_org.sh ${projectname} "$bugid"f
-
-echo "removing source: /tmp/sources/$projectname/${bugid}f"
-rm -rf /tmp/sources/$projectname/"$bugid"f
 
 done
--- scripts/Mockito/Metric_run.sh
+++ scripts/Mockito/Metric_run.sh
@@ -7,15 +7,9 @@ for i in {1..38}
 do
 bugid="$i"
 
-echo "checking out source"
-mkdir -p /tmp/sources/$projectname/"$bugid"f
-defects4j checkout -p $projectname -v "$bugid"f -w /tmp/sources/$projectname/"$bugid"f
-
 echo "collecting metrics"
 echo "./metric_collection_org.sh ${projectname} ${bugid}f"
 ../metric_collection_org.sh ${projectname} "$bugid"f
 
-echo "removing source: /tmp/sources/$projectname/${bugid}f"
-rm -rf /tmp/sources/$projectname/"$bugid"f
 
 done
