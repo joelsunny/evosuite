d3c5f5eb5aa6ed151f1844afc447f1aead1533da
joel sunny <joelsunny.ch@gmail.com>
2020-12-04 16:14:45 +0530

    scripts

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -2,13 +2,15 @@ import pandas as pd
 import sys
 import os
 
-def filter(in_file):
+def filter(in_file, projectname, bugid):
     data = pd.read_csv(in_file)
     mean_val=data.iloc[0]["mean_score_of_loaded_classes"]
     median_val=data.iloc[0]["median_score_of_loaded_classes"]
-    print("mean = " + str(mean_val) + ",  median = " + str(median_val))
+    print(projectname + "," + str(bugid) + "," + str(mean_val) + "," + str(median_val))
     
 
 if __name__ == "__main__":
     in_file = sys.argv[1]
-    filter(in_file)
+    projectname = sys.argv[2]
+    bugid = sys.argv[3]
+    filter(in_file, projectname, bugid)
